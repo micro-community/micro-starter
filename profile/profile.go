cd280ca85ec5046cab4deff0ef6a1f8fdbfb15d2
Edward <crazybber@outlook.com>
2020-10-15 13:57:16 +0800

    just all in micro & remove go-micro library completely

--- profile/profile.go
+++ profile/profile.go
@@ -1,28 +1,26 @@
 package profile
 
 import (
-	"github.com/micro/go-micro/v3/auth/noop"
-	"github.com/micro/go-micro/v3/broker/http"
-	"github.com/micro/go-micro/v3/registry/mdns"
-	"github.com/micro/go-micro/v3/runtime/local"
-	"github.com/micro/go-micro/v3/store/file"
+	"github.com/micro/micro/v3/service/auth"
+	"github.com/micro/micro/v3/service/auth/noop"
+	"github.com/micro/micro/v3/service/broker/http"
+	"github.com/micro/micro/v3/service/config"
+	"github.com/micro/micro/v3/service/config/env"
+	"github.com/micro/micro/v3/service/events"
+	"github.com/micro/micro/v3/service/events/stream/memory"
 	"github.com/micro/micro/v3/service/logger"
+	"github.com/micro/micro/v3/service/registry/mdns"
+	"github.com/micro/micro/v3/service/runtime"
+	"github.com/micro/micro/v3/service/runtime/local"
+	"github.com/micro/micro/v3/service/store"
+	"github.com/micro/micro/v3/service/store/file"
 
-	mEnv "github.com/micro/go-micro/v3/config/env"
-	memStream "github.com/micro/go-micro/v3/events/stream/memory"
-
-	microProfile "github.com/micro/micro/v3/profile"
-	microAuth "github.com/micro/micro/v3/service/auth"
-	microConfig "github.com/micro/micro/v3/service/config"
-	microEvents "github.com/micro/micro/v3/service/events"
-	microRuntime "github.com/micro/micro/v3/service/runtime"
-	microStore "github.com/micro/micro/v3/service/store"
-
+	"github.com/micro/micro/v3/profile"
 	"github.com/urfave/cli/v2"
 )
 
 func init() {
-	microProfile.Register("dev", Dev)
+	profile.Register("dev", Dev)
 }
 
 /*
@@ -33,19 +31,19 @@ func init() {
 */
 
 // Dev profile to run develop env
-var Dev = &microProfile.Profile{
+var Dev = &profile.Profile{
 	Name: "dev",
 	Setup: func(ctx *cli.Context) error {
-		microAuth.DefaultAuth = noop.NewAuth()
-		microRuntime.DefaultRuntime = local.NewRuntime()
-		microStore.DefaultStore = file.NewStore()
-		//microStore.DefaultStore = mem.NewStore()
-		microConfig.DefaultConfig, _ = mEnv.NewConfig()
-		microProfile.SetupBroker(http.NewBroker())
-		microProfile.SetupRegistry(mdns.NewRegistry())
-		//	microProfile.SetupJWTRules()
+		auth.DefaultAuth = noop.NewAuth()
+		runtime.DefaultRuntime = local.NewRuntime()
+		store.DefaultStore = file.NewStore()
+		//store.DefaultStore = mem.NewStore()
+		config.DefaultConfig, _ = env.NewConfig()
+		profile.SetupBroker(http.NewBroker())
+		profile.SetupRegistry(mdns.NewRegistry())
+		//	profile.SetupJWTRules()
 		var err error
-		microEvents.DefaultStream, err = memStream.NewStream()
+		events.DefaultStream, err = memory.NewStream()
 		if err != nil {
 			logger.Fatalf("Error configuring stream for dev: %v", err)
 		}
